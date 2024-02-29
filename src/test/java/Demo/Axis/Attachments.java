e3fbeb6d835077f5e618fef22717f5d1f899e7b4
Rohit RG <rohitrg000@gmail.com>
2024-02-29 17:48:16 +0530

    Updated

--- src/test/java/Demo/Axis/Attachments.java
+++ src/test/java/Demo/Axis/Attachments.java
@@ -1,47 +1,40 @@
 package Demo.Axis;
 
 
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterMethod;
 
-public class Attachments extends Login {
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.testng.annotations.AfterMethod;
 
-	@AfterMethod
-	public void addAttachments() {
+	public class Attachments extends Login{
 
-		WebDriver driver = Login.driver;
-		WebElement imme = driver.findElement(By.xpath("//a[contains(text(),'Immigration')]"));
-		imme.click();
-		System.out.println("Clicked on Immegration");
+		@AfterMethod
+		public void AddAttach() {
+			WebDriver driver = Login.driver;
+			WebElement immi=driver.findElement(By.xpath("(//div[@class=\"orangehrm-tabs-wrapper\"])[5]"));
+			immi.click();
+			System.out.println("Clicked on Immigration");
+			WebElement Addattach = driver.findElement(By.xpath("(//button[@class=\"oxd-button oxd-button--medium oxd-button--text\"])[2]"));
+			Addattach.click();
+			System.out.println("Clicked on Add Attachemnts");
+			WebElement Browse = driver.findElement(By.xpath("(//input[@type='file'])[1]"));
+			Browse.sendKeys("C:\\Users\\rohit\\Downloads\\QA Axis Bank\\New Text Document.txt");
+			
+			WebElement comments = driver.findElement(By.xpath("//textarea[@placeholder='Type comment here']"));
+			comments.click();
+			comments.sendKeys("File Uploaded");
+			System.out.println("Box");
+			WebElement save = driver.findElement(By.xpath("//button[normalize-space()='Save']"));
+			save.click();
+			System.out.println("Attachment is Saved");
 
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-
-		WebElement ele = driver.findElement(By.xpath("(//button[@type='button'][normalize-space()='Add'])[2]"));
-		js.executeScript("arguments[0].scrollIntoView(true);", ele);
-		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-		ele.click();
-		System.out.println("Clicked on Add Attachments Button");
-
-		
-		WebElement file = driver.findElement(By.xpath("(//input[@type='file'])[1]"));
-		file.sendKeys("C:\\Users\\rohit\\Downloads\\QA Axis Bank\\New Text Document.txt");
-		 
-		WebElement comments = driver.findElement(By.xpath("//textarea[@placeholder='Type comment here']"));
-		comments.click();
-		comments.sendKeys("Text File Uploaded");
-		System.out.println("Clicked on Comments Box ");
-		 
-		WebElement save = driver.findElement(By.xpath("//button[normalize-space()='Save']"));
-		save.click();
-		System.out.println("Attachment is Saved");
-		 
-		
-		 
-
+			
+			
 	}
+	
+			
+		
 }
+
--- src/test/java/Demo/Axis/Job.java
+++ src/test/java/Demo/Axis/Job.java
@@ -1,5 +1,7 @@
 package Demo.Axis;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,10 +14,12 @@ public class Job extends Login{
 
 		WebDriver driver = Login.driver;
 		WebElement job = driver.findElement(By.xpath("//a[normalize-space()='Job']"));
+		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
 		job.click();
 		System.out.println("Clicked on Job");
 		
 		WebElement slider = driver.findElement(By.xpath("//div[@class='oxd-switch-wrapper']"));
+		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
 		slider.click();
 		System.out.println("Clicked on button now its showing Contract Details");
 
